6d4e6770722cf45da7c0ec6e34a39b656acb74e7
gozlen <lukasz.zajaczkowski9@gmail.com>
2021-04-24 19:59:34 +0200

    added big and large

--- connection_test.py
+++ connection_test.py
@@ -1,6 +1,6 @@
 import requests
 
-data = '{"sender": "test_user1", "message": "what are the advantages of small leaves"}'
+data = '{"sender": "test_user1", "message": "what are the advantages of big leaves"}'
 # data = '{"sender": "test_user1", "message": "tell me about low sun environment"}'
 url = "http://206.189.104.159:5005/webhooks/rest/webhook"
 response = requests.post(url, data)
