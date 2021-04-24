18afef40bfd2a04f8a6ed0f7e56aa5e6cd1097c3
gozlen <lukasz.zajaczkowski9@gmail.com>
2021-04-24 19:00:05 +0200

    adding sun for sunlight and rain for rainfall

--- connection_test.py
+++ connection_test.py
@@ -1,6 +1,7 @@
 import requests
 
-data = '{"sender": "test_user1", "message": "tell me about high rainfall environment"}'
+data = '{"sender": "test_user1", "message": "tell me about high wind environment"}'
+# data = '{"sender": "test_user1", "message": "tell me about low sun environment"}'
 url = "http://206.189.104.159:5005/webhooks/rest/webhook"
 response = requests.post(url, data)
 print(response.content)
