5711f737c7834bfbb4236e4e63f31723ce4c5343
gozlen <lukasz.zajaczkowski9@gmail.com>
2021-05-01 23:48:16 +0200

    added more plurals, fixed nlu fallbacks

--- connection_test.py
+++ connection_test.py
@@ -1,6 +1,6 @@
 import requests
 
-data = '{"sender": "test_user1", "message": "what leaves in low sun"}'
+data = '{"sender": "sucker", "message": "what leaves in heavy rain"}'
 # data = '{"sender": "test_user1", "message": "tell me about low sun environment"}'
 url = "http://206.189.104.159:5005/webhooks/rest/webhook"
 response = requests.post(url, data)
